ed919b16dde702082d8837a6d633e4bdfc4b04de
ceccs17d38 <42964237+ceccs17d38@users.noreply.github.com>
2018-11-29 09:55:30 +0530

    Update 31_circular_queue_LL.c

--- 31_circular_queue_LL.c
+++ 31_circular_queue_LL.c
@@ -1,201 +1,201 @@
-//Name        : Nirmal K George Mathew
-//Roll no.    : 38
-//Program no. : 31
-//program name: Circular queue using linkedlist
+       //Name        : Nirmal K George Mathew
+       //Roll no.    : 38
+       //Program no. : 31
+       //program name: Circular queue using linkedlist
 
-#include<stdio.h>
-#include<stdlib.h>
-#define que struct queue
-#define pf printf
-#define sf scanf
-struct queue{
-int info;
-struct queue *link;
-};
- que *front=NULL,*rear=NULL;
-int count=0;
-void push(int n)
-{
-que *newnode;
-newnode=(struct queue*)malloc(sizeof(struct queue));
-newnode->info=n;
-newnode->link=NULL;
-if(count==0)
-front=newnode;
-else
-        rear->link=newnode;
-    rear=newnode;
-    rear->link=front;
-count++;
-}
-int pop(void)
-{
-int n;
-que *temp;
-if(count==0)
-return (-1);
-count--;
-    if(front==rear)
-    {
-        n=front->info;
-        free(front);
-        front=NULL;
-        rear=NULL;
-    }else
-    {
-            temp= front ;
-            n = temp-> info ;
-            front = front -> link ;
-            rear -> link = front ;
-            free ( temp ) ;
-    }
-return n;
-}
-void display(void)
-{
-que *temp;
-int i;
-if(count==0)
-pf("Empty");
-else
-{
-temp=front;
-for(i=0;i<count;i++)
-{
-pf("%d ",temp->info);
-temp=temp->link;
-}
-}
-pf("\n");
-}
-int size(void)
-{
-return count;
-}
-int main()
-{
-int n,ch=10;
-while(ch!=0)
-{
-pf("\n       What do you want to do??\n");
-pf("1.Push\n");
-pf("2.Pop\n");
-pf("3.SizeOfQueue\n");
-pf("4.Display\n");
-pf("0.EXIT\n");
-sf("%d",&ch);
-switch(ch)
-{
-case 1:
-{
-pf("What no. do you want to push in queue\n");
-sf("%d",&n);
-push(n);
-break;
-}
-case 2:
-{
-n=pop();
-if(n==-1)
-pf("Queue is empty\n");
-else
-pf("Number poped from queue is %d\n",n);
-break;
-}
-case 3:
-{
-n=size();
-pf("Size of queue is %d\n",n);
-break;
-}
-case 4:
-{
-pf("Queue is -->> ");
-display();
-}
-case 0:
-break;
-default:
-pf("Wrong Choice\n");
-break;
-}
-}
-}
-
-
-OUTPUT:
+       #include<stdio.h>
+       #include<stdlib.h>
+       #define que struct queue
+       #define pf printf
+       #define sf scanf
+       struct queue{
+       int info;
+       struct queue *link;
+       };
+        que *front=NULL,*rear=NULL;
+       int count=0;
+       void push(int n)
+       {
+       que *newnode;
+       newnode=(struct queue*)malloc(sizeof(struct queue));
+       newnode->info=n;
+       newnode->link=NULL;
+       if(count==0)
+       front=newnode;
+       else
+               rear->link=newnode;
+           rear=newnode;
+           rear->link=front;
+       count++;
+       }
+       int pop(void)
+       {
+       int n;
+       que *temp;
+       if(count==0)
+       return (-1);
+       count--;
+           if(front==rear)
+           {
+               n=front->info;
+               free(front);
+               front=NULL;
+               rear=NULL;
+           }else
+           {
+                   temp= front ;
+                   n = temp-> info ;
+                   front = front -> link ;
+                   rear -> link = front ;
+                   free ( temp ) ;
+           }
+       return n;
+       }
+       void display(void)
+       {
+       que *temp;
+       int i;
+       if(count==0)
+       pf("Empty");
+       else
+       {
+       temp=front;
+       for(i=0;i<count;i++)
+       {
+       pf("%d ",temp->info);
+       temp=temp->link;
+       }
+       }
+       pf("\n");
+       }
+       int size(void)
+       {
+       return count;
+       }
+       int main()
+       {
+       int n,ch=10;
+       while(ch!=0)
+       {
+       pf("\n       What do you want to do??\n");
+       pf("1.Push\n");
+       pf("2.Pop\n");
+       pf("3.SizeOfQueue\n");
+       pf("4.Display\n");
+       pf("0.EXIT\n");
+       sf("%d",&ch);
+       switch(ch)
+       {
+       case 1:
+       {
+       pf("What no. do you want to push in queue\n");
+       sf("%d",&n);
+       push(n);
+       break;
+       }
+       case 2:
+       {
+       n=pop();
+       if(n==-1)
+       pf("Queue is empty\n");
+       else
+       pf("Number poped from queue is %d\n",n);
+       break;
+       }
+       case 3:
+       {
+       n=size();
+       pf("Size of queue is %d\n",n);
+       break;
+       }
+       case 4:
+       {
+       pf("Queue is -->> ");
+       display();
+       }
+       case 0:
+       break;
+       default:
+       pf("Wrong Choice\n");
+       break;
+       }
+       }
+       }
 
 
-What do you want to do??
-1.Push
-2.Pop
-3.SizeOfQueue
-4.Display
-0.EXIT
-1
-What no. do you want to push in queue
-4
+       OUTPUT:
+
 
        What do you want to do??
-1.Push
-2.Pop
-3.SizeOfQueue
-4.Display
-0.EXIT
-1
-What no. do you want to push in queue
-6
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       1
+       What no. do you want to push in queue
+       4
 
-       What do you want to do??
-1.Push
-2.Pop
-3.SizeOfQueue
-4.Display
-0.EXIT
-1
-What no. do you want to push in queue
-5
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       1
+       What no. do you want to push in queue
+       6
 
-       What do you want to do??
-1.Push
-2.Pop
-3.SizeOfQueue
-4.Display
-0.EXIT
-3
-Size of queue is 3
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       1
+       What no. do you want to push in queue
+       5
 
-       What do you want to do??
-1.Push
-2.Pop
-3.SizeOfQueue
-4.Display
-0.EXIT
-4
-Queue is -->> 4 6 5
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       3
+       Size of queue is 3
 
-       What do you want to do??
-1.Push
-2.Pop
-3.SizeOfQueue
-4.Display
-0.EXIT
-2
-Number poped from queue is 4
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       4
+       Queue is -->> 4 6 5
 
-       What do you want to do??
-1.Push
-2.Pop
-3.SizeOfQueue
-4.Display
-0.EXIT
-2
-Number poped from queue is 6
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       2
+       Number poped from queue is 4
 
-       What do you want to do??
-1.Push
-2.Pop
-3.SizeOfQueue
-4.Display
-0.EXIT
-0
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       2
+       Number poped from queue is 6
+
+              What do you want to do??
+       1.Push
+       2.Pop
+       3.SizeOfQueue
+       4.Display
+       0.EXIT
+       0
