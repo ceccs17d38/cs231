77eb9277c5dafc4b512f8b3426e77f1ad2725cde
ceccs17d38 <42964237+ceccs17d38@users.noreply.github.com>
2018-11-29 09:34:18 +0530

    Update 05_mergesortWR.c

--- 05_mergesortWR.c
+++ 05_mergesortWR.c
@@ -1,87 +1,87 @@
-//Name        : Nirmal K George Mathew
-//Roll no.    : 38
-//Program no. : 5
-//program name: Mergesort without Recursion
+	//Name        : Nirmal K George Mathew
+	//Roll no.    : 38
+	//Program no. : 5
+	//program name: Mergesort without Recursion
 
-#include <stdio.h>
+	#include <stdio.h>
 
-int main()
-{
-	int arr[30],temp[30],i,j,k,n,size,l1,h1,l2,h2;
-
-	printf("Enter the number of elements : ");
-	scanf("%d",&n);
-
-	for(i=0;i<n;i++)
+	int main()
 	{
-		printf("Enter element %d : ",i+1);
-		scanf("%d",&arr[i]);
-	}
+		int arr[30],temp[30],i,j,k,n,size,l1,h1,l2,h2;
 
-	printf("Unsorted list is : ");
-	for( i = 0 ; i<n ; i++)
-		printf("%d ", arr[i]);
-	for(size=1; size < n; size=size*2 )
-	{
-		l1=0;
-		k=0;  
-		while( l1+size < n)
-		{
-			h1=l1+size-1;
-			l2=h1+1;
-			h2=l2+size-1;
-			if( h2>=n ) 
-				h2=n-1;
-			i=l1;
-			j=l2;
-			
-			while(i<=h1 && j<=h2 )
-			{
-				if( arr[i] <= arr[j] )
-					temp[k++]=arr[i++];
-				else
-					temp[k++]=arr[j++];
-			}
-			
-			while(i<=h1)
-				temp[k++]=arr[i++];
-			while(j<=h2)
-				temp[k++]=arr[j++];
-			l1=h2+1; 
-		}
-		for(i=l1; k<n; i++) 
-			temp[k++]=arr[i];
+		printf("Enter the number of elements : ");
+		scanf("%d",&n);
 
 		for(i=0;i<n;i++)
-			arr[i]=temp[i];
+		{
+			printf("Enter element %d : ",i+1);
+			scanf("%d",&arr[i]);
+		}
 
-		printf("\nElements are : ");
+		printf("Unsorted list is : ");
 		for( i = 0 ; i<n ; i++)
 			printf("%d ", arr[i]);
-		
+		for(size=1; size < n; size=size*2 )
+		{
+			l1=0;
+			k=0;  
+			while( l1+size < n)
+			{
+				h1=l1+size-1;
+				l2=h1+1;
+				h2=l2+size-1;
+				if( h2>=n ) 
+					h2=n-1;
+				i=l1;
+				j=l2;
+
+				while(i<=h1 && j<=h2 )
+				{
+					if( arr[i] <= arr[j] )
+						temp[k++]=arr[i++];
+					else
+						temp[k++]=arr[j++];
+				}
+
+				while(i<=h1)
+					temp[k++]=arr[i++];
+				while(j<=h2)
+					temp[k++]=arr[j++];
+				l1=h2+1; 
+			}
+			for(i=l1; k<n; i++) 
+				temp[k++]=arr[i];
+
+			for(i=0;i<n;i++)
+				arr[i]=temp[i];
+
+			printf("\nElements are : ");
+			for( i = 0 ; i<n ; i++)
+				printf("%d ", arr[i]);
+
+		}
+
+		printf("Sorted list is :\n");
+		for( i = 0 ; i<n ; i++)
+			printf("%d ", arr[i]);
+
+		printf("\n");
+
+		return 0;
 	}
-	
-	printf("Sorted list is :\n");
-	for( i = 0 ; i<n ; i++)
-		printf("%d ", arr[i]);
-	
-	printf("\n");
-	
-	return 0;
-}
 
 
 
-OUTPUT:
+	OUTPUT:
 
 
-	Enter the number of elements : 4
-	Enter element 1 : 5
-	Enter element 2 : 9
-	Enter element 3 : 3
-	Enter element 4 : 2
-	Unsorted list is : 5 9 3 2 
-	Elements are : 5 9 2 3 
-	Elements are : 2 3 5 9 Sorted list is :
-	2 3 5 9 
+		Enter the number of elements : 4
+		Enter element 1 : 5
+		Enter element 2 : 9
+		Enter element 3 : 3
+		Enter element 4 : 2
+		Unsorted list is : 5 9 3 2 
+		Elements are : 5 9 2 3 
+		Elements are : 2 3 5 9 Sorted list is :
+		2 3 5 9 
 
