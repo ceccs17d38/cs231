2962cfb3ea0d3ef7f4456aaa35397e8711a6c44b
ceccs17d38 <42964237+ceccs17d38@users.noreply.github.com>
2018-11-29 09:32:32 +0530

    Update 02_selection_sort.c

--- 02_selection_sort.c
+++ 02_selection_sort.c
@@ -1,58 +1,58 @@
-//Name        : Nirmal K George Mathew
-//Roll no.    : 38
-//Program no. : 2
-//program name: Selection sort
+	//Name        : Nirmal K George Mathew
+	//Roll no.    : 38
+	//Program no. : 2
+	//program name: Selection sort
 
-#include <stdio.h>
+	#include <stdio.h>
 
-int selectionSort(int array[], int size) {
-	int i, j, pos, temp;
-	for (i=0; i<size; i++) {
-		pos = i;
+	int selectionSort(int array[], int size) {
+		int i, j, pos, temp;
+		for (i=0; i<size; i++) {
+			pos = i;
 
-		for (j=i; j<size; j++) {
-			if (array[j] < array[pos]) {
-				pos =j;
+			for (j=i; j<size; j++) {
+				if (array[j] < array[pos]) {
+					pos =j;
+				}
+			}
+			if (i != pos) {
+				temp = array[i];
+				array[i] = array[pos];
+				array[pos] = temp;
 			}
 		}
-		if (i != pos) {
-			temp = array[i];
-			array[i] = array[pos];
-			array[pos] = temp;
+	}
+
+	int main() {
+		int i, j, pos, array[100], temp, size;
+
+		printf("Enter number of elements in the array : ");
+		scanf("%d", &size);
+
+		printf("Enter the array\n");
+		for (i=0; i<size; i++) {
+			scanf("%d", &array[i]);
+		}
+
+		selectionSort(array, size);
+
+		printf("Sorted array \n");
+		for (i=0; i<size; i++) {
+			printf("%d\n", array[i]);
 		}
 	}
-}
-
-int main() {
-	int i, j, pos, array[100], temp, size;
-
-	printf("Enter number of elements in the array : ");
-	scanf("%d", &size);
-
-	printf("Enter the array\n");
-	for (i=0; i<size; i++) {
-		scanf("%d", &array[i]);
-	}
-	
-	selectionSort(array, size);
-
-	printf("Sorted array \n");
-	for (i=0; i<size; i++) {
-		printf("%d\n", array[i]);
-	}
-}
 
 
 
-OUTPUT:
-	Enter number of elements in the array : 4
-	Enter the array
-	4
-	6
-	2
-	1
-	Sorted array 
-	1
-	2
-	4
-	6
+	OUTPUT:
+		Enter number of elements in the array : 4
+		Enter the array
+		4
+		6
+		2
+		1
+		Sorted array 
+		1
+		2
+		4
+		6
