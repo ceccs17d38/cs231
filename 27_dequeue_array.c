4e07f26fb114d81d6d4f245a30a6a590278ef459
ceccs17d38 <42964237+ceccs17d38@users.noreply.github.com>
2018-11-29 09:51:18 +0530

    Update 27_dequeue_array.c

--- 27_dequeue_array.c
+++ 27_dequeue_array.c
@@ -1,251 +1,251 @@
-//Name        : Nirmal K George Mathew
-//Roll no.    : 38
-//Program no. : 27
-//program name: Dequeue using array
+	//Name        : Nirmal K George Mathew
+	//Roll no.    : 38
+	//Program no. : 27
+	//program name: Dequeue using array
 
-#include <stdio.h>
+	#include <stdio.h>
 
-int queue[100], n=100, front=-1, rear=-1;
+	int queue[100], n=100, front=-1, rear=-1;
 
-void enqueueRear() {
-	int data;
-	if (rear == n-1) {
-		printf("Failed: Queue full\n");
-	} else {
-		printf("Enter data to be entered : ");
+	void enqueueRear() {
+		int data;
+		if (rear == n-1) {
+			printf("Failed: Queue full\n");
+		} else {
+			printf("Enter data to be entered : ");
+			scanf("%d", &data);
+			if (front == -1) {
+				front = rear = 0;
+			} else {
+				rear++;
+			}
+			queue[rear] = data;
+		}
+	}
+	void enqueueFront() {
+	    int data;
+	    if (front == 0) {
+		printf("Failed: No space at front\n");
+	    } else {
+		printf("Enter data to be inserted : ");
 		scanf("%d", &data);
 		if (front == -1) {
-			front = rear = 0;
+		    front = rear = 0;
 		} else {
-			rear++;
+		    front--;
 		}
-		queue[rear] = data;
+		queue[front] = data;
+	    }
 	}
-}
-void enqueueFront() {
-    int data;
-    if (front == 0) {
-        printf("Failed: No space at front\n");
-    } else {
-        printf("Enter data to be inserted : ");
-        scanf("%d", &data);
-        if (front == -1) {
-            front = rear = 0;
-        } else {
-            front--;
-        }
-        queue[front] = data;
-    }
-}
-void dequeueRear() {
-    if (rear == -1) {
-        printf("Failed: Empty queue\n");
-    } else {
-        printf("Deleted element : %d\n", queue[rear]);
-        if (front == rear) {
-            front = rear = -1;
-        } else {
-            rear--;
-        }
-    }
-}
-void dequeueFront() {
-	if (front == -1) {
+	void dequeueRear() {
+	    if (rear == -1) {
 		printf("Failed: Empty queue\n");
-	} else {
-		printf("Deleted element : %d\n", queue[front]);
+	    } else {
+		printf("Deleted element : %d\n", queue[rear]);
 		if (front == rear) {
-			front = rear = -1;
+		    front = rear = -1;
 		} else {
-			front++;
+		    rear--;
+		}
+	    }
+	}
+	void dequeueFront() {
+		if (front == -1) {
+			printf("Failed: Empty queue\n");
+		} else {
+			printf("Deleted element : %d\n", queue[front]);
+			if (front == rear) {
+				front = rear = -1;
+			} else {
+				front++;
+			}
 		}
 	}
-}
-void printQueue() {
-	int i=0;
-	if (front == -1) {
-		printf("Empty Queue\n");
-	} else {
-		for (i=front; i<=rear; i++) {
-			printf("%d\n", queue[i]);
+	void printQueue() {
+		int i=0;
+		if (front == -1) {
+			printf("Empty Queue\n");
+		} else {
+			for (i=front; i<=rear; i++) {
+				printf("%d\n", queue[i]);
+			}
 		}
 	}
-}
-int main() {
-	char choice;
-	front = rear = -1;
-	do {
-		printf("\n\tQueue using array\n");
-        printf("1.Enqueue Front\n");
-        printf("2.Enqueue Rear\n");
-        printf("3.Dequeue Front\n");
-        printf("4.Dequeue Rear\n");
-        printf("5.Display\n");
-        printf("6.Exit\n");
-        printf("Enter your choice : ");
-		scanf(" %c", &choice);
-		switch(choice) {
-			case '1':
-				enqueueFront();
-				break;
-			case '2':
-				enqueueRear();
-				break;
-			case '3':
-				dequeueFront();
-				break;
-			case '4':
-				dequeueRear();
-				break;
-			case '5':
-				printQueue();
-				break;
-		}
-	} while(choice != '6');
-}
+	int main() {
+		char choice;
+		front = rear = -1;
+		do {
+			printf("\n\tQueue using array\n");
+		printf("1.Enqueue Front\n");
+		printf("2.Enqueue Rear\n");
+		printf("3.Dequeue Front\n");
+		printf("4.Dequeue Rear\n");
+		printf("5.Display\n");
+		printf("6.Exit\n");
+		printf("Enter your choice : ");
+			scanf(" %c", &choice);
+			switch(choice) {
+				case '1':
+					enqueueFront();
+					break;
+				case '2':
+					enqueueRear();
+					break;
+				case '3':
+					dequeueFront();
+					break;
+				case '4':
+					dequeueRear();
+					break;
+				case '5':
+					printQueue();
+					break;
+			}
+		} while(choice != '6');
+	}
 
 
-OUTPUT:
+	OUTPUT:
 
 
-    Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 1
-Enter data to be inserted : 3
+	    Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 1
+	Enter data to be inserted : 3
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 1
-Failed: No space at front
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 1
+	Failed: No space at front
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 4
-Deleted element : 3
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 4
+	Deleted element : 3
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 2
-Enter data to be entered : 4
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 2
+	Enter data to be entered : 4
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 5
-4
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 5
+	4
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 3
-Deleted element : 4
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 3
+	Deleted element : 4
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 5
-Empty Queue
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 5
+	Empty Queue
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 1
-Enter data to be inserted : 5
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 1
+	Enter data to be inserted : 5
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 2
-Enter data to be entered : 5
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 2
+	Enter data to be entered : 5
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 2
-Enter data to be entered : 6
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 2
+	Enter data to be entered : 6
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 5
-5
-5
-6
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 5
+	5
+	5
+	6
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 4
-Deleted element : 6
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 4
+	Deleted element : 6
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 5
-5
-5
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 5
+	5
+	5
 
-        Queue using array
-1.Enqueue Front
-2.Enqueue Rear
-3.Dequeue Front
-4.Dequeue Rear
-5.Display
-6.Exit
-Enter your choice : 6
+		Queue using array
+	1.Enqueue Front
+	2.Enqueue Rear
+	3.Dequeue Front
+	4.Dequeue Rear
+	5.Display
+	6.Exit
+	Enter your choice : 6
