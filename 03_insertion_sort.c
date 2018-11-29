af24f877eb2f6b8d82b2a1e9e0720164dfc66cf1
ceccs17d38 <42964237+ceccs17d38@users.noreply.github.com>
2018-11-29 09:33:08 +0530

    Update 03_insertion_sort.c

--- 03_insertion_sort.c
+++ 03_insertion_sort.c
@@ -1,50 +1,50 @@
-//Name        : Nirmal K George Mathew
-//Roll no.    : 38
-//Program no. : 3
-//program name: Insertion sort
+	//Name        : Nirmal K George Mathew
+	//Roll no.    : 38
+	//Program no. : 3
+	//program name: Insertion sort
 
-#include <stdio.h>
+	#include <stdio.h>
 
-int insertionSort(int array[], int size) {
-	int i, j, temp;
-	for (i=1; i<size; i++) {
-		temp = array[i];
-		for (j=i-1; j>=0 && array[j] > temp; j--) {
-				array[j+1] = array[j];
+	int insertionSort(int array[], int size) {
+		int i, j, temp;
+		for (i=1; i<size; i++) {
+			temp = array[i];
+			for (j=i-1; j>=0 && array[j] > temp; j--) {
+					array[j+1] = array[j];
+			}
+			array[j+1] = temp;
 		}
-		array[j+1] = temp;
 	}
-}
-void main() {
-	int array[100], i, j, size;
+	void main() {
+		int array[100], i, j, size;
 
-	printf("Enter size of the array : ");
-	scanf("%d", &size);
+		printf("Enter size of the array : ");
+		scanf("%d", &size);
 
-	printf("Enter array\n");
-	for (i=0; i<size; i++) {
-		scanf("%d", &array[i]);
+		printf("Enter array\n");
+		for (i=0; i<size; i++) {
+			scanf("%d", &array[i]);
+		}
+
+		insertionSort(array, size);
+
+		printf("Sorted array\n");
+		for (i=0; i<size; i++) {
+			printf("%d\n", array[i]);
+		}
 	}
 
-	insertionSort(array, size);
-	
-	printf("Sorted array\n");
-	for (i=0; i<size; i++) {
-		printf("%d\n", array[i]);
-	}
-}
 
+	OUTPUT:
 
-OUTPUT:
-
-	Enter size of the array : 4
-	Enter array
-	5
-	7
-	2
-	6
-	Sorted array
-	2
-	5
-	6
-	7
+		Enter size of the array : 4
+		Enter array
+		5
+		7
+		2
+		6
+		Sorted array
+		2
+		5
+		6
+		7
