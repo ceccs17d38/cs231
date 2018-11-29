161ce4f942e81d625282c805577f8b8863aa7fae
ceccs17d38 <42964237+ceccs17d38@users.noreply.github.com>
2018-11-29 09:46:03 +0530

    Update 19_stack_LL.c

--- 19_stack_LL.c
+++ 19_stack_LL.c
@@ -1,117 +1,117 @@
-//Name        : Nirmal K George Mathew
-//Roll no.    : 38
-//Program no. : 19
-//program name: Stack using linkedlist
+	//Name        : Nirmal K George Mathew
+	//Roll no.    : 38
+	//Program no. : 19
+	//program name: Stack using linkedlist
 
-#include<stdio.h>
-#include<stdlib.h>
-struct node
-{
-    	int data;
-    	struct node *link;
-};
-void main()
-{
-    	int c;
-    	struct node *temp,*T,*head;
-    	temp=(struct node*)malloc(sizeof(struct node));
-    	temp->data=0;
-    	temp->link=NULL;
-    	head=temp;
-    	T=head->link;
-    	do
+	#include<stdio.h>
+	#include<stdlib.h>
+	struct node
 	{
-        	printf("\nCHOOSE\n1.PUSH\n2.POP\n3.DISPLAY\n");
-        	scanf("%d",&c);
-		switch(c)
+		int data;
+		struct node *link;
+	};
+	void main()
+	{
+		int c;
+		struct node *temp,*T,*head;
+		temp=(struct node*)malloc(sizeof(struct node));
+		temp->data=0;
+		temp->link=NULL;
+		head=temp;
+		T=head->link;
+		do
 		{
-        		case 1:
-       			{
-            			temp=(struct node*)malloc(sizeof(struct node));
-            			printf("enter the element: ");
-            			scanf("%d",&temp->data);
-            			temp->link=head->link;
-            			head->link=temp;
-            			T=head->link;
-				break;
-        		}
-        		case 2:
-        		{
-            			if(T==NULL)
-            			{	
-                			printf("stack is empty!!!");
-            			}
-            			else
-            			{
-                			printf("popped out: %d",T->data);
-                			temp=T;
-                			T=T->link;
-                			head->link=T;
-                			free(temp);
-            			}
-				break;
-        		}
-        		case 3:
-        		{
-            			printf("elements: ");
-            			temp=T;
-            			while(temp!=NULL)
-            			{
-                			printf("%d ->",temp->data);
-                			temp=temp->link;
-            			}
-            			printf("NULL");
-				break;
-        		}
-			default:
+			printf("\nCHOOSE\n1.PUSH\n2.POP\n3.DISPLAY\n");
+			scanf("%d",&c);
+			switch(c)
 			{
-				printf("invalid choice");
+				case 1:
+				{
+					temp=(struct node*)malloc(sizeof(struct node));
+					printf("enter the element: ");
+					scanf("%d",&temp->data);
+					temp->link=head->link;
+					head->link=temp;
+					T=head->link;
+					break;
+				}
+				case 2:
+				{
+					if(T==NULL)
+					{	
+						printf("stack is empty!!!");
+					}
+					else
+					{
+						printf("popped out: %d",T->data);
+						temp=T;
+						T=T->link;
+						head->link=T;
+						free(temp);
+					}
+					break;
+				}
+				case 3:
+				{
+					printf("elements: ");
+					temp=T;
+					while(temp!=NULL)
+					{
+						printf("%d ->",temp->data);
+						temp=temp->link;
+					}
+					printf("NULL");
+					break;
+				}
+				default:
+				{
+					printf("invalid choice");
+				}
 			}
-		}
-	}while(c==1 || c==2 || c==3);
-    
-}
+		}while(c==1 || c==2 || c==3);
+
+	}
 
 
-OUTPUT:
+	OUTPUT:
 
 
-CHOOSE
-1.PUSH
-2.POP
-3.DISPLAY
-1
-enter the element: 4
+	CHOOSE
+	1.PUSH
+	2.POP
+	3.DISPLAY
+	1
+	enter the element: 4
 
-CHOOSE
-1.PUSH
-2.POP
-3.DISPLAY
-1
-enter the element: 5
+	CHOOSE
+	1.PUSH
+	2.POP
+	3.DISPLAY
+	1
+	enter the element: 5
 
-CHOOSE
-1.PUSH
-2.POP
-3.DISPLAY
-1
-enter the element: 6
+	CHOOSE
+	1.PUSH
+	2.POP
+	3.DISPLAY
+	1
+	enter the element: 6
 
-CHOOSE
-1.PUSH
-2.POP
-3.DISPLAY
-2
-popped out: 6
-CHOOSE
-1.PUSH
-2.POP
-3.DISPLAY
-2
-popped out: 5
-CHOOSE
-1.PUSH
-2.POP
-3.DISPLAY
-3
-elements: 4 ->NULL
+	CHOOSE
+	1.PUSH
+	2.POP
+	3.DISPLAY
+	2
+	popped out: 6
+	CHOOSE
+	1.PUSH
+	2.POP
+	3.DISPLAY
+	2
+	popped out: 5
+	CHOOSE
+	1.PUSH
+	2.POP
+	3.DISPLAY
+	3
+	elements: 4 ->NULL
