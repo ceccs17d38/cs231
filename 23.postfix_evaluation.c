d7e1ee1d976b990107383f8583671d81982e38b6
ceccs17d38 <42964237+ceccs17d38@users.noreply.github.com>
2018-11-29 09:49:01 +0530

    Update 23.postfix_evaluation.c

--- 23.postfix_evaluation.c
+++ 23.postfix_evaluation.c
@@ -1,60 +1,60 @@
-//Name        : Nirmal K George Mathew
-//Roll no.    : 38
-//Program no. : 23
-//program name: Postfix evaluation
+  //Name        : Nirmal K George Mathew
+  //Roll no.    : 38
+  //Program no. : 23
+  //program name: Postfix evaluation
 
-#include <stdio.h>
-#include <stdlib.h>
-int power(int base, int exponent) {
-  if (exponent==1) {
-    return 1;
-  } else {
-    return base*power(base, exponent-1);
-  }
-}
-int evaluvate(int operand1, int operator, int operand2) {
-  switch(operator) {
-    case '+':
-      return (operand1+operand2);
-    case '-':
-      return (operand1-operand2);
-    case '*':
-      return (operand1*operand2);
-    case '/':
-      return (operand1/operand2);
-    case '^':
-      return (power(operand1, operand2));
-    default:
-      printf("Invalid operator");
-      exit(0);
-  }
-}
-int isOperand(char symbol) {
-  if ((symbol>=48 && symbol<=57)) {
-    return 1;
-  }
-  return 0;
-}
-int main() {
-  char postfix[20];
-  int stack[100];
-  int i, top=-1, x, y;
-  printf("Enter the postfix expression : ");
-  scanf(" %s", &postfix);
-  for (i=0; postfix[i] != '\0'; i++) {
-    if (isOperand(postfix[i])) {
-      stack[++top] = postfix[i]-48;
+  #include <stdio.h>
+  #include <stdlib.h>
+  int power(int base, int exponent) {
+    if (exponent==1) {
+      return 1;
     } else {
-      y = stack[top--];
-      x = stack[top];
-      stack[top] = evaluvate(x, postfix[i], y);
+      return base*power(base, exponent-1);
     }
   }
-  printf("%d\n", stack[top]);
-}
+  int evaluvate(int operand1, int operator, int operand2) {
+    switch(operator) {
+      case '+':
+        return (operand1+operand2);
+      case '-':
+        return (operand1-operand2);
+      case '*':
+        return (operand1*operand2);
+      case '/':
+        return (operand1/operand2);
+      case '^':
+        return (power(operand1, operand2));
+      default:
+        printf("Invalid operator");
+        exit(0);
+    }
+  }
+  int isOperand(char symbol) {
+    if ((symbol>=48 && symbol<=57)) {
+      return 1;
+    }
+    return 0;
+  }
+  int main() {
+    char postfix[20];
+    int stack[100];
+    int i, top=-1, x, y;
+    printf("Enter the postfix expression : ");
+    scanf(" %s", &postfix);
+    for (i=0; postfix[i] != '\0'; i++) {
+      if (isOperand(postfix[i])) {
+        stack[++top] = postfix[i]-48;
+      } else {
+        y = stack[top--];
+        x = stack[top];
+        stack[top] = evaluvate(x, postfix[i], y);
+      }
+    }
+    printf("%d\n", stack[top]);
+  }
 
 
-OUTPUT:
+  OUTPUT:
 
-Enter the postfix expression : 123+*
-5
+  Enter the postfix expression : 123+*
+  5
